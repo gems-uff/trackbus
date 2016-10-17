3945524c418fd0998d0ec877b1fff9688b23531d
Raissa Amaral <raissa_amaral@id.uff.br>
2016-10-16 22:19:40 -0200

    adicionando parser e arquivos dos pontos

--- www/js/stops_equivalence/stops_equivalence.controllers.js
+++ www/js/stops_equivalence/stops_equivalence.controllers.js
@@ -6,19 +6,59 @@
         .controller('StopsEquivalenceController', StopsEquivalenceController);
 
     StopsEquivalenceController.$inject = [
-        'Papa', 'busSpatialService', 'BUS'
+        'Papa', 'busSpatialService', 'BUS', '$q'
     ];
 
     function StopsEquivalenceController(
-        Papa, busSpatialService, BUS
+        Papa, busSpatialService, BUS, $q
     ) {
 
-        activate();
+        var result_file;
+        var line_stops_file = ('data_files/pontos_por_linha.csv'); 
+        var stops_description_file = ('data_files/stops_description.csv');
 
-        function activate(argument) {
-            console.log(Papa);
-            console.log(busSpatialService);
+        var line_stops 
+        var stops_description;
+        
+        start();
+
+
+        function start() {
+            parseFiles().then(matchStops);
+        };
+
+        function fileParseError(result) {
+            alert("ERROR reading the file");
+        };
+
+        function fileParser(file) {
+            return Papa.parse(file, {
+                download: true,
+            })
+            .then(function(result){
+                return result.data;
+            })
+            .catch(fileParseError)
+        };
+
+        function parseFiles() {
+            var promises = [];
+            promises.push(
+                fileParser(line_stops_file).then(function(result){
+                    line_stops = result;
+                })
+            );
+            promises.push(
+                fileParser(stops_description_file).then(function(result){
+                    stops_description = result;
+                })
+            );
+            return $q.all(promises);
+        };  
+
+
+        function matchStops(){
+            debugger
         };
     };
-
 })();
